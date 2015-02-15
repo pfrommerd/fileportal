9388343d42c83e464906650ee9a1fc3e42e9da70
samuel <sam@ASJB2s-MacBook-Pro-3.local>
2015-02-15 00:59:06 -0500

    Added accept messages

--- src/fileportal/gui/AcceptNotification.java
+++ src/fileportal/gui/AcceptNotification.java
@@ -1,24 +1,104 @@
 package fileportal.gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.notification.Notification;
+import com.notification.NotificationBuilder;
 import com.notification.types.ComponentNotification;
+import com.theme.TextTheme;
+import com.theme.ThemePackage;
 
 public class AcceptNotification extends ComponentNotification {
+	private JButton m_accept;
+	private JButton m_decline;
+	private JLabel m_text;
+
+	private SynchronizeObject m_obj = new SynchronizeObject();
+	private boolean m_accepted = false;
+
+	private class SynchronizeObject {
+
+	}
+
 	public AcceptNotification(String text) {
 		JPanel customPanel = new JPanel();
-		JButton accept = new JButton("Accept");
-		JButton decline = new JButton("Decline");
-		
-		customPanel.add(accept);
-		customPanel.add(decline);
-		
-		addComponent(new JLabel(text), BorderLayout.NORTH);
+		m_accept = new JButton("Accept");
+		m_decline = new JButton("Decline");
+
+		m_accept.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				m_accepted = true;
+				hide();
+				synchronized (m_obj) {
+					m_obj.notify();
+				}
+			}
+		});
+
+		m_decline.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				m_accepted = false;
+				hide();
+				synchronized (m_obj) {
+					m_obj.notify();
+				}
+			}
+		});
+
+		customPanel.add(m_accept);
+		customPanel.add(m_decline);
+		m_text = new JLabel(text);
+
+		addComponent(m_text, BorderLayout.NORTH);
 		addComponent(customPanel, BorderLayout.CENTER);
 		setSize(300, 70);
+	}
+
+	public void setText(String text) {
+		m_text.setText(text);
+	}
+
+	public void setTextTheme(TextTheme theme) {
+		m_text.setFont(theme.subtitle);
+		m_accept.setFont(theme.subtitle);
+		m_decline.setFont(theme.subtitle);
+	}
+
+	/**
+	 * @return whether or not the file was accepted
+	 */
+	public boolean getAccept() {
+		try {
+			synchronized (m_obj) {
+				m_obj.wait();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return m_accepted;
+	}
+
+	public static class AcceptNotificationBuilder implements
+			NotificationBuilder {
+		@Override
+		public Notification buildNotification(ThemePackage pack, Object[] args) {
+			AcceptNotification note = null;
+			if (args.length != 0) {
+				note = new AcceptNotification((String) args[0]);
+			} else {
+				note = new AcceptNotification("");
+			}
+			note.setWindowTheme(pack.windowTheme);
+			return note;
+		}
 	}
 }
--- src/fileportal/gui/PortalApp.java
+++ src/fileportal/gui/PortalApp.java
@@ -18,6 +18,14 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+import com.notification.NotificationFactory;
+import com.notification.NotificationFactory.PopupLocation;
+import com.notification.NotificationManager;
+import com.notification.QueueManager;
+import com.notification.Time;
+import com.theme.ThemePackagePresets;
+
+import fileportal.gui.AcceptNotification.AcceptNotificationBuilder;
 import fileportal.net.Discoverer;
 import fileportal.net.FileReceiverServer;
 import fileportal.net.ReceiverHandler;
@@ -33,79 +41,79 @@ public class PortalApp extends JFrame {
 	public static final int TAB_HEIGHT = 100;
 	public static final int PANEL_WIDTH = 300;
 	public static final int PANEL_HEIGHT = 300;
-	
+
 	public static final int Y_OFFSET = 30;
-	
+
 	public static final int MOVE_SPEED = 20;
-	
+
 	public static final int SCROLL_SPEED = 6;
 
 	public static final int GRID_SPACING = 10;
-	
+
 	public static BufferedImage DEFAULT_USER_ICON = null;
 
 	public static BufferedImage PROFILE_SETTINGS_ICON = null;
-	
+
 	public static final int PROFILE_BAR_HEIGHT = 32;
 	public static final int PROFILE_NAME_X_OFF = 5;
 	public static final int PROFILE_NAME_Y_OFF = 10;
-	
-	//Should be the same as hover radius
+
+	// Should be the same as hover radius
 	public static final int USER_ICON_TOP_SPACE = 20;
-	
+
 	public static final int USER_ICON_WIDTH = 50;
 	public static final int USER_ICON_HEIGHT = 50;
-	
+
 	public static final int USER_ICON_HOVER_RADIUS = 20;
 	public static final int USER_ICON_HOVER_SPEED = 3;
-	
+
 	public static final int USER_NAME_SPACING = 10;
 	public static final int USER_MAX_NAME_WIDTH = 70;
 	public static final int USER_NAME_LINE_HEIGHT = 20;
-	
+
 	public static final int USER_NAME_MAX_CHARS = 10;
-	
+
 	public static final int DIVIDER_THICKNESS = 1;
 	public static final Color DIVIDER_COLOR = Color.GRAY;
 
 	public static final Font PROFILE_FONT = new Font("Dialog", Font.BOLD, 16);
 	public static final Font USER_FONT = new Font("Dialog", Font.BOLD, 12);
-	
+
 	public static final Color FONT_COLOR = Color.GRAY;
-	
+
 	public static int SCREEN_WIDTH;
 	public static int SCREEN_HEIGHT;
-	
-	
-	
+
 	static {
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice defaultScreen = ge.getDefaultScreenDevice();
-        Rectangle rect = defaultScreen.getDefaultConfiguration().getBounds();
-        
+		GraphicsEnvironment ge = GraphicsEnvironment
+				.getLocalGraphicsEnvironment();
+		GraphicsDevice defaultScreen = ge.getDefaultScreenDevice();
+		Rectangle rect = defaultScreen.getDefaultConfiguration().getBounds();
+
 		SCREEN_WIDTH = (int) rect.getWidth();
 		SCREEN_HEIGHT = (int) rect.getHeight();
-		
+
 		try {
-			DEFAULT_USER_ICON = ImageIO.read(PortalApp.class.getResourceAsStream("/unknown-user.png"));
-			PROFILE_SETTINGS_ICON = ImageIO.read(PortalApp.class.getResourceAsStream("/gear.png"));
+			DEFAULT_USER_ICON = ImageIO.read(PortalApp.class
+					.getResourceAsStream("/unknown-user.png"));
+			PROFILE_SETTINGS_ICON = ImageIO.read(PortalApp.class
+					.getResourceAsStream("/gear.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}	
-	
+	}
+
 	private User m_user;
 	private DiscoveryPanel m_discoveryPanel;
-	
+
 	public PortalApp(User user) {
 		m_user = user;
-				
+
 		setLocation(SCREEN_WIDTH - PANEL_WIDTH - TAB_WIDTH, Y_OFFSET);
 		setSize(PANEL_WIDTH + TAB_WIDTH, PANEL_HEIGHT);
 
 		setUndecorated(true);
 		setAlwaysOnTop(true);
-		
 
 		JPanel main = new JPanel() {
 			public void paintComponent(Graphics g) {
@@ -115,50 +123,51 @@ public class PortalApp extends JFrame {
 						RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 				g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 						RenderingHints.VALUE_ANTIALIAS_ON);
-			    g2d.setRenderingHint(
-			            RenderingHints.KEY_FRACTIONALMETRICS,
-			            RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-			    g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-			    
+				g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,
+						RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+				g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+						RenderingHints.VALUE_RENDER_QUALITY);
+
 				super.paintComponent(g);
 			}
 		};
 		main.setLayout(new BorderLayout());
 		JPanel subPanel = new JPanel();
-		
+
 		subPanel.setLayout(new BorderLayout());
 
 		m_discoveryPanel = new DiscoveryPanel();
-		
+
 		JScrollPane scroll = new JScrollPane();
 		scroll.getVerticalScrollBar().setUnitIncrement(SCROLL_SPEED);
 		scroll.setBorder(null);
-		
+
 		scroll.getViewport().add(m_discoveryPanel);
-		
+
 		subPanel.add(scroll, BorderLayout.CENTER);
 		subPanel.add(new ProfileBar(m_user), BorderLayout.NORTH);
-		
+
 		main.add(new PulloutTabPanel(this), BorderLayout.WEST);
 		main.add(subPanel, BorderLayout.CENTER);
-		
+
 		add(main);
 		setVisible(true);
 	}
-	
+
 	public boolean isPanelShowing() {
 		return getX() + TAB_WIDTH < SCREEN_WIDTH;
 	}
-	
+
 	public void addDiscoverer(Discoverer d) {
 		d.addHandler(m_discoveryPanel);
 	}
+
 	public void removeDiscoverer(Discoverer d) {
 		d.removeHandler(m_discoveryPanel);
 	}
-	
+
 	public void run() {
-		while(true) {
+		while (true) {
 			repaint();
 			try {
 				Thread.sleep(50);
@@ -167,72 +176,81 @@ public class PortalApp extends JFrame {
 			}
 		}
 	}
-	
+
 	public void hidePanel() {
-		while(isPanelShowing()) {
-			//Move to the right
-			setLocation(getX()  + MOVE_SPEED, getY());
+		while (isPanelShowing()) {
+			// Move to the right
+			setLocation(getX() + MOVE_SPEED, getY());
 			repaint();
 		}
 	}
-	
+
 	public void showPanel() {
-		while(getX() > SCREEN_WIDTH - PANEL_WIDTH - TAB_WIDTH) {
-			//Move to the left
+		while (getX() > SCREEN_WIDTH - PANEL_WIDTH - TAB_WIDTH) {
+			// Move to the left
 			setLocation(getX() - MOVE_SPEED, getY());
 			repaint();
 		}
 		repaint();
 	}
-	
+
 	public static void main(String[] args) {
 		User user = new User(System.getProperty("user.name"));
 		user.setIcon(DEFAULT_USER_ICON);
-		/*NotificationManager manager = new SimpleManager(PopupLocation.SOUTHEAST);
 
-		AcceptNotification notification = new AcceptNotification("Testing notifications");
-		notification.setWindowTheme(ThemePackagePresets.cleanLight().windowTheme);
-		manager.addNotification(notification, Time.seconds(3));*/
-		
-		
+		final NotificationFactory noteFactory = new NotificationFactory(
+				ThemePackagePresets.cleanLight());
+		final NotificationManager noteManager = new QueueManager(
+				PopupLocation.NORTHWEST);
+		noteFactory.addBuilder("accept", new AcceptNotificationBuilder());
+
 		LANDiscoverer disc = new LANDiscoverer(user);
 		LANBroadcaster broad = new LANBroadcaster(user);
-		
-		
+
 		LANIconServer icon = new LANIconServer(user);
 		icon.start();
-		
+
 		FileReceiverServer server = new FileReceiverServer(
 				new ReceiverHandler() {
 					@Override
 					public boolean shouldAccept(String user, String name) {
+						AcceptNotification note = (AcceptNotification) noteFactory
+								.build("accept", user + " wants to share "
+										+ name + " with you.");
+						noteManager.addNotification(note, Time.seconds(5));
+						return true;
+					}
+
+					@Override
+					public boolean shouldAccept(String user, int fileNum) {
+						AcceptNotification note = (AcceptNotification) noteFactory
+								.build("accept", user + " wants to share "
+										+ fileNum + " files with you.");
+						noteManager.addNotification(note, Time.seconds(5));
 						return true;
 					}
 
 					@Override
 					public File getFileSaveLocation(String name) {
-						return new File(System.getProperty("user.home") + "/Desktop/" + name);
+						return new File(System.getProperty("user.home")
+								+ "/Desktop/" + name);
 					}
 
 					@Override
 					public File getFolderSaveLocation() {
-						return new File(System.getProperty("user.home") + "/Desktop/");
-					}
-
-					@Override
-					public boolean shouldAccept(String user, int fileNum) {
-						return true;
+						return new File(System.getProperty("user.home")
+								+ "/Desktop/");
 					}
 				});
 		server.start();
-		
+
 		disc.start();
 		broad.start();
-		
+
 		PortalApp app = new PortalApp(user);
 		app.addDiscoverer(disc);
-		
-		//will run the main loop
+
+		// will run the main loop
 		app.run();
 	}
 }
